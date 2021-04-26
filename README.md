720f8232710c2fe77a3e1779f02c775da1818019
Grant Goldman <grantgoldman0@gmail.com>
2021-04-26 11:44:31 -0400

    add docs

--- README.md
+++
@@ -1,5 +0,0 @@
-# mama
-
-Run mama_ldscores.py to generate LD scores, and then run mama.py to run the MAMA method.
-
-[Work in progress]
---
+++ tutorial/1.run_mama_ldscores.sh
@@ -0,0 +1,6 @@
+python3 ../mama_ldscores.py --ances-path "./LDSC_input/iid_ances_file" \
+                            --snp-ances "./LDSC_input/snp_ances_file" \
+                            --ld-wind-cm 1 \
+                            --stream-stdout \
+                            --bfile-merged-path "./LDSC_input/chr22_mind02_geno02_maf01_EAS_EUR" \
+                            --out "chr22_mind02_geno02_maf01_EAS_EUR"
